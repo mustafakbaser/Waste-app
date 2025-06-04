46e679aba81ca1e4b9821417ff524c37dfe94c9c
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2025-06-04 14:03:12 +0300

    Add core application structure and entry points

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SelectSkipPage from './pages/SelectSkipPage/SelectSkipPage';
+
+function App() {
+  return (
+    <SelectSkipPage />
+  );
+}
+
+export default App;
